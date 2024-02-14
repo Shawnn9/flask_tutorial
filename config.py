6029e5733cf0eed915bb85503929adcda2328ffd
Shawn <shawneliezer@gmail.com>
2024-02-14 10:35:47 -0800

    fixed the things u asked

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import secrets
 
 class Config:
-    DEBUG = True
-    TESTING = False
     SECRET_KEY = secrets.token_hex(16)
     MONGO_URI = 'mongodb://localhost:27017/your_database'
     VERSION = "1.0.0.0"
