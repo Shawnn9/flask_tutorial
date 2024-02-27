6bd4f974c09c1c36dd3e04f53b753b268c6318cb
Shawn <shawneliezer@gmail.com>
2024-02-27 10:17:36 -0800

    changed the name controller

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from controllers.analytics import analytics_bp
+from config import config
 from controllers.auth import auth_bp
 from controllers.main import main_bp
 
 app = Flask(__name__, static_url_path='static')
-app.secret_key = 'your_secret_key'
+app.secret_key = app.config['SECRET_KEY']
 app.register_blueprint(auth_bp)
 app.register_blueprint(main_bp)
 app.register_blueprint(analytics_bp)
