cebe4b0b032a49b9ce3b62b39954e4c757201a41
Shawn <shawneliezer@gmail.com>
2024-03-03 15:32:30 -0800

    added table to the analytics file and fixed the problem

--- controllers/analytics.py
+++ controllers/analytics.py
@@ -1,25 +1,25 @@
 from flask import Blueprint, render_template, session, request, redirect, url_for
-from models.user import User  # Assuming you have a user model defined
+from models.user import User
 
 analytics_bp = Blueprint('analytics', __name__, static_url_path='/static', template_folder='templates')
 
-@analytics_bp.route('/analytics/')
+@analytics_bp.route('/analytics')
 def analytics():
-    users = User.find()  # Assuming you have implemented a method to retrieve users asynchronously
+    users = User.find()
     session_name = session.get('username', 'Guest')
     return render_template('analytics.html', users=users, session_name=session_name)
 
 @analytics_bp.route('/delete_user/', methods=['POST'])
 def delete_user():
-    if 'username' in session:  # Ensure user is logged in
-        username_to_delete = request.form.get('username')
-        User.delete_one({'username': username_to_delete})  # Assuming you have implemented a method to delete a user asynchronously
+    if 'username' in session:
+        username_to_delete = request.form['username']
+        User.delete_user({'username': username_to_delete})
     return redirect(url_for('analytics.analytics'))
 
 @analytics_bp.route('/update_password/', methods=['POST'])
 def update_password():
-    if 'username' in session:  # Ensure user is logged in
-        username = request.form.get('username')
-        new_password = request.form.get('password')
-        User.update_one({'username': username}, {'$set': {'password': new_password}})  # Assuming you have implemented a method to update password asynchronously
+    if 'username' in session:
+        username = request.form['username']
+        new_password = request.form['new_password']
+        User.update_password({'username': username}, {'$set': {'password': new_password}})
     return redirect(url_for('analytics.analytics'))
