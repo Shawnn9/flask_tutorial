34622e59ec4333bc52632687487e3af58fc546ab
Shawn <shawneliezer@gmail.com>
2024-03-13 13:11:43 +0200

    changed the file in dal and models

--- controllers/auth.py
+++ controllers/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, redirect, url_for
 from models.user import User
 from beanie import init_beanie
 
--- dal/entities/user.py
+++ dal/entities/user.py
@@ -2,7 +2,6 @@ from beanie import Document
 from beanie import init_beanie
 from pydantic import EmailStr
 
-from controllers.auth import connect_to_db
 
 class User(Document):
     username: str
